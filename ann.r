00c050754806a7f4c95b5c08b6b6d8615388597d
Sudipsamanta1 <54319964+Sudipsamanta1@users.noreply.github.com>
2019-09-19 13:04:45 +0530

    Update ann.r

--- ann.r
+++ ann.r
@@ -1,19 +1,18 @@
-### running ann on ll-all data
 
+### running ann on ll-all data
 library(nnet)
-leuko_df <- read.csv("/home/sudip/summer-tranning-project/dataset/leukemia_small")
-#irisdata<-iris
+leuko_data <- read.csv("leukemia-selected-100.csv")
+
 #The neural network requires that the species be normalized using one-of-n normalization. We will normalize between 0 and 1.  
 #setwd("/home/mca100/rwp")
-#setwd("~/summer-tranning-project/r code")
 
 #Breaking the data into train and test data
-trainDataIndx = sample(1:3572,3000)
-testDataIndx = setdiff(1:3572,trainDataIndx)
+trainDataIndx = sample(1:72,52)
+testDataIndx = setdiff(1:72,trainDataIndx)
 
-trainData <- mat[trainDataIndx,]
-testData <- mat[testDataIndx,]
-mat <- as.matrix(mat)
+trainData <- leuko_data[trainDataIndx,]
+testData <- leuko_data[testDataIndx,]
+#mat <- as.matrix(mat)
 
 xTrain <- subset(trainData, select=-class)
 yTrain <- subset(trainData, select=class)
@@ -21,11 +20,10 @@ yTrain <- subset(trainData, select=class)
 xTest <- subset(testData, select=-class)
 yTest <- subset(testData, select=class)
 
-ideal <- class.ind(mat$class)
+ideal <- class.ind(leuko_data$class)
 
-ann_model = nnet(xTrain, ideal[trainDataIndx,], size=25, softmax=TRUE)
+ann_model = nnet(xTrain, ideal[trainDataIndx,], size=9, softmax=TRUE)
 
-pred <- predict(ann_model,leuko_df[testDataIndx,-5], type="class")
+pred <- predict(ann_model, leuko_data[testDataIndx,-5], type="class")
 
 table(pred,unlist(yTest))
-
